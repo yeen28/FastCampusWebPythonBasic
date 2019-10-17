e702f457beed09206686ddc4e3d8ebbe122bc03f
yeen28 <48883549+yeen28@users.noreply.github.com>
2019-10-17 17:15:09 +0900

    Update main.py

--- Learning/Python/Course06/code/main.py
+++ Learning/Python/Course06/code/main.py
@@ -1,15 +1,27 @@
-test1 = "Life is too short, %s %s %s"
+test1 = "Life is too short, %s"
 result1 = test1 % "You need python."
+print(result1) # Life is too short, You need python.
+test1 = "Life is too short, %s %s %s"
 result1 = test1 % ("You", "need", "python.")
-print(result1)
+print(result1) # Life is too short, You need python.
 test2 = "Life is too short, %s"
 result2 = test2 % "10"
-print(result2)
+print(result2) # Life is too short, 10
+# 하지만 
+# test2 = "Life is too short, %d"
+# result2 = test2 % "10"  -> 에러!
 test3 = "Life is too short, %.2f"
 result3 = test3 % 100.1234
-print(result3)
+print(result3) # Life is too short, 100.12
 test4 = "Life is too short, {} {} {}"
 result4 = test4.format("You", "need", 10)
-print(result4)
+print(result4) # Life is too short, You need 10
+# test4 = "Life is too short, %s %s %s"
+# result4 = test4.format("You", "need", 10)
+# print(result4) # Life is too short, %s %s %s
 test5 = "\"Life is too short\",\n\'You need python.\'"
 print(test5)
+'''
+"Life is too short",
+'You need python.'
+'''
