7a691f87616380bd6050036650fd730cf4d2e1a0
yeen28 <48883549+yeen28@users.noreply.github.com>
2019-10-19 11:52:34 +0900

    Update main.py

--- Learning/Python/Course07/code/main.py
+++ Learning/Python/Course07/code/main.py
@@ -3,19 +3,26 @@ def sum(x, y, test = 1):
 
 x = 1
 y = 1
-print(sum(x, y, 3))
+print(sum(x, y, 3)) # 5
 
 def sum(x, *y):
     result = 0
     for i in y:
         result += i
     print(x + result)
+    
+sum(1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10) # 56
 
-sum(1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+'''
+def sum(x, *y):
+    sum_list = []
+    for i in y:
+        sum_list.append(i)
+    print(sum_list)
+sum(1, 1, 2, 3, 4, 5)  # [1, 2, 3, 4, 5]
+'''
 
 def sum(one, two):
     return one + two
 
-print(sum(one = 1, two = 1))
-
-
+print(sum(one = 1, two = 1)) # 2
